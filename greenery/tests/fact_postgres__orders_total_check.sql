a3168bb9c14ac44a3117ddce5cfd4a882c01af03
Simon Edwards <simon.edwards@sony.com>
2023-04-21 11:50:26 +0000

    Added total_check singular test

---
+++ greenery/tests/fact_postgres__orders_total_check.sql
@@ -0,0 +1,13 @@
+WITH fact_orders AS (
+  SELECT
+    *
+  FROM {{ source('fact_postgres', 'fact_postgres__orders') }}
+)
+
+SELECT
+    ORDER_ID,
+    SUM(ORDER_COST + SHIPPING_COST)::text AS ORDER_TOTAL_CALC,
+    SUM(ORDER_TOTAL)::text AS ORDER_TOTAL_ACTUAL
+FROM fact_orders
+GROUP BY 1
+HAVING NOT (ORDER_TOTAL_CALC = ORDER_TOTAL_ACTUAL)
